6afdd04a79e561e7f49c1ff57faf9e5bcc56116d
MORHERO <m.rimkus1997@gmail.com>
2021-05-03 01:35:33 +0200

    little design optimization in equipment tab

--- modules/parts/character_equipment.php
+++ modules/parts/character_equipment.php
@@ -2,43 +2,93 @@
 	<div class="flex">
 		<div class="main">
 
-			<h2>Inventar</h2>
-			<div submodule="accordion" class="clean">
+			<div submodule="accordion" class="">
 				<div class="flex" id="equipment_main_content">
 					
 					<div class="element">
 						<div task="accordion_title">
-							<p>Gegenständename</p>
+							<p>Inventar</p>
 						</div>
 						<div task="accordion_content" class="">
-							<div class="flex" item="equipment">
-								<div>
-									<span item="skill-title">test</span>
+							<div item="equipment">
+								<div class="flex base-section">
+									<p item="equipment-title">Gegenständename</p>
+									<p item="equipment-rarity">(Seltenheit)</p>
+									<div class="flex">
+										<input class="btn-clean" item="equipment-amount" type="number" value="1">
+										<div class="settings-btn">
+											<button icon="decrease" onclick=""></button>
+										</div>
+										<div class="settings-btn">
+											<button icon="increase" onclick=""></button>
+										</div>
+									</div>
+									<div class="flex">
+										<div class="settings-btn disabled">
+											<button icon="skill-edit"></button>
+										</div>
+										<div class="settings-btn disabled">
+											<button icon="skill-remove" onclick=""></button>
+										</div>
+									</div>
 								</div>
-								<div>
-
+								<div class="flex info-section">
+									<div>
+										<p>Größe: <span item="equipment-size">20cm</span></p>
+										<p>Gewicht: <span item="equipment-weight">1.2kg</span></p>
+										<p>Wert: <span item="equipment-value">2 G</span></p>
+									</div>
+									<div>
+										<p>Zustand: <span item="equipment-condition">Neu</span></p>
+										<p>Typ: <span item="equipment-type">Trank</span></p>
+									</div>
+									<div>
+										<textarea item="equipment-description"></textarea>
+									</div>
+								</div>
+							</div>
+							<div item="equipment">
+								<div class="flex base-section">
+									<p item="equipment-title">Gegenständename</p>
+									<p item="equipment-rarity">(Seltenheit)</p>
+									<div class="flex">
+										<input class="btn-clean" item="equipment-amount" type="number" value="1">
+										<div class="settings-btn">
+											<button icon="decrease" onclick=""></button>
+										</div>
+										<div class="settings-btn">
+											<button icon="increase" onclick=""></button>
+										</div>
+									</div>
+									<div class="flex">
+										<div class="settings-btn disabled">
+											<button icon="skill-edit"></button>
+										</div>
+										<div class="settings-btn disabled">
+											<button icon="skill-remove" onclick=""></button>
+										</div>
+									</div>
+								</div>
+								<div class="flex info-section">
+									<div>
+										<p>Größe: <span item="equipment-size">20cm</span></p>
+										<p>Gewicht: <span item="equipment-weight">1.2kg</span></p>
+										<p>Wert: <span item="equipment-value">2 G</span></p>
+									</div>
+									<div>
+										<p>Zustand: <span item="equipment-condition">Neu</span></p>
+										<p>Typ: <span item="equipment-type">Trank</span></p>
+									</div>
+									<div>
+										<textarea item="equipment-description"></textarea>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 					<div class="element">
 						<div task="accordion_title">
-							<p>Gegenständename</p>
-						</div>
-						<div task="accordion_content" class="">
-							<div class="flex" item="equipment">
-								<div>
-									<span item="skill-title"></span>
-								</div>
-								<div>
-
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="element">
-						<div task="accordion_title">
-							<p>Gegenständename</p>
+							<p>Eingelagerte Waren</p>
 						</div>
 						<div task="accordion_content" class="">
 							<div class="flex" item="equipment">
@@ -55,57 +105,87 @@
 				</div>
 			</div>
 
-			<h2>Eingelagert</h2>
-			<h2>Inventar-erweiterung</h2>
 		</div>
 		<div class="sub">
 			<div id="money_content">
-				<h2>Geld</h2>
+				<div class="heading">
+					<p>Geld</p>
+				</div>
 				<div class="flex">
 					<div item="money_main">
 						<p>Verfügbar:</p>
 						<div class="group" item="money_platin">
-							<p>Platin <span>(10G)</span></p>
-							<p item="money_amount"><input type="number" value="0" />P</p>
+							<div class="flex">
+								<p>Platin</p>
+								<p item="money_amount"><input type="number" value="0" />P</p>
+							</div>
+							<p class="info">(1P = 10G)</p>
 						</div>
 						<div class="group" item="money_gold">
-							<p>Gold <span>(100S)</span></p>
-							<p item="money_amount"><input type="number" value="0" />G</p>
+							<div class="flex">
+								<p>Gold</p>
+								<p item="money_amount"><input type="number" value="0" />G</p>
+							</div>
+							<p class="info">(1G = 100S)</p>
 						</div>
 						<div class="group" item="money_silver">
-							<p>Silber <span>(10B)</span></p>
-							<p item="money_amount"><input type="number" value="0" />S</p>
+							<div class="flex">
+								<p>Silber</p>
+								<p item="money_amount"><input type="number" value="0" />S</p>
+							</div>
+							<p class="info">(1S = 10B)</p>
 						</div>
 						<div class="group" item="money_bronze">
-							<p>Bronze <span>(100K)</span></p>
-							<p item="money_amount"><input type="number" value="0" />B</p>
+							<div class="flex">
+								<p>Bronze</p>
+								<p item="money_amount"><input type="number" value="0" />B</p>
+							</div>
+							<p class="info">(1B = 100K)</p>
 						</div>
 						<div class="group" item="money_copper">
-							<p>Kupfer</p>
-							<p item="money_amount"><input type="number" value="0" />K</p>
+							<div class="flex">
+								<p>Kupfer</p>
+								<p item="money_amount"><input type="number" value="0" />K</p>
+							</div>
+							<p class="info">&nbsp;</p>
 						</div>
 					</div>
 					<div item="money_bank">
 						<p>Bank:</p>
 						<div class="group" item="money_platin">
-							<p>Platin <span>(10G)</span></p>
-							<p item="money_amount"><input type="number" value="0" />P</p>
+							<div class="flex">
+								<p>Platin</p>
+								<p item="money_amount"><input type="number" value="0" />P</p>
+							</div>
+							<p class="info">(1P = 10G)</p>
 						</div>
 						<div class="group" item="money_gold">
-							<p>Gold <span>(100S)</span></p>
-							<p item="money_amount"><input type="number" value="0" />G</p>
+							<div class="flex">
+								<p>Gold</p>
+								<p item="money_amount"><input type="number" value="0" />G</p>
+							</div>
+							<p class="info">(1G = 100S)</p>
 						</div>
 						<div class="group" item="money_silver">
-							<p>Silber <span>(10B)</span></p>
-							<p item="money_amount"><input type="number" value="0" />S</p>
+							<div class="flex">
+								<p>Silber</p>
+								<p item="money_amount"><input type="number" value="0" />S</p>
+							</div>
+							<p class="info">(1S = 10B)</p>
 						</div>
 						<div class="group" item="money_bronze">
-							<p>Bronze <span>(100K)</span></p>
-							<p item="money_amount"><input type="number" value="0" />B</p>
+							<div class="flex">
+								<p>Bronze</p>
+								<p item="money_amount"><input type="number" value="0" />B</p>
+							</div>
+							<p class="info">(1B = 100K)</p>
 						</div>
 						<div class="group" item="money_copper">
-							<p>Kupfer</p>
-							<p item="money_amount"><input type="number" value="0" />K</p>
+							<div class="flex">
+								<p>Kupfer</p>
+								<p item="money_amount"><input type="number" value="0" />K</p>
+							</div>
+							<p class="info">&nbsp;</p>
 						</div>
 					</div>
 				</div>
